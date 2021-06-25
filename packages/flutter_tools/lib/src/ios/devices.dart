8ac4aa7f3eb2b06229398fc984dcee9883af5436
CHI-HSIUNG YANG <sensbear@gmail.com>
2021-06-25 14:40:29 +0800

    Update devices.dart
    fixed Waiting for observatory port to be available...

--- packages/flutter_tools/lib/src/ios/devices.dart
+++ packages/flutter_tools/lib/src/ios/devices.dart
@@ -432,7 +432,7 @@ class IOSDevice extends Device {
       }
 
       _logger.printTrace('Application launched on the device. Waiting for observatory url.');
-      final Timer timer = Timer(discoveryTimeout ?? const Duration(seconds: 30), () {
+      final Timer timer = Timer(discoveryTimeout ?? const Duration(seconds: 60), () {
         _logger.printError('iOS Observatory not discovered after 30 seconds. This is taking much longer than expected...');
       });
       final Uri localUri = await observatoryDiscovery.uri;
